24e57e5ac205a9002b38a9b8e1517d1e8951c215
karlen <karlenderawan07@gmail.com>
2023-09-30 22:48:11 +0800

    Final

--- script.js
+++ script.js
@@ -43,6 +43,88 @@
     }
   }
 
-  // Panggil fungsi pertama kali untuk memulai pengetikan
+  // Panggil fMessageungsi pertama kali untuk memulai pengetikan
   typeText();
-})();
+})()
+
+
+// Click Scroll
+
+// Mengambil semua link menu yang memiliki href yang dimulai dengan '#'
+const menuLinks = document.querySelectorAll('nav ul li a[href^="#"]');
+
+// Menambahkan event listener ke setiap link menu
+menuLinks.forEach((link) => {
+    link.addEventListener('click', smoothScroll);
+});
+
+// Menambahkan event listener untuk mengawasi perubahan posisi scroll
+window.addEventListener('scroll', toggleActiveClass);
+
+// Fungsi untuk membuat efek scroll halus
+function smoothScroll(e) {
+    e.preventDefault();
+
+    // Mengambil target dari link yang diklik
+    const targetId = this.getAttribute('href').substring(1);
+
+    // Mengambil elemen target dengan ID yang sesuai
+    const targetElement = document.getElementById(targetId);
+
+    if (!targetElement) return;
+
+    // Menghitung posisi scroll target element
+    const targetPosition = targetElement.getBoundingClientRect().top;
+
+    // Menghitung posisi scroll saat ini
+    const startScroll = window.scrollY;
+
+    // Animasi scroll
+    const startTime = performance.now();
+    const duration = 500; // Durasi animasi dalam milidetik
+
+    function scroll(time) {
+        const elapsedTime = time - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
+        const newPosition = startScroll + (targetPosition * progress);
+
+        window.scrollTo(0, newPosition);
+
+        if (progress < 1) {
+            requestAnimationFrame(scroll);
+        }
+    }
+
+    requestAnimationFrame(scroll);
+}
+
+// Fungsi untuk mengaktifkan atau menonaktifkan kelas pada menu navigasi
+function toggleActiveClass() {
+    const scrollPosition = window.scrollY;
+
+    // Menggunakan loop untuk memeriksa setiap link menu
+    menuLinks.forEach((link) => {
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (!targetElement) return;
+
+        // Menghitung posisi scroll target element
+        const targetPosition = targetElement.getBoundingClientRect().top;
+        const targetHeight = targetElement.offsetHeight;
+
+        // Memeriksa apakah posisi scroll saat ini berada di dalam section
+        if (scrollPosition >= targetPosition && scrollPosition < targetPosition + targetHeight) {
+            // Menghapus kelas "active" dari semua link menu
+            menuLinks.forEach((link) => {
+                link.classList.remove('active');
+            });
+
+            // Menambahkan kelas "active" pada link menu yang sesuai
+            link.classList.add('active');
+        }
+    });
+}
+
+
+
